6fe3cb532b8a2810e9914f8eb8cbb4538ebb79fc
Marc's <Marc's@Nel-PC>
2013-06-10 23:21:01 -0600

    Actualizacion sobre el menu

--- application/views/includes/header_ci.php
+++ application/views/includes/header_ci.php
@@ -1,4 +1,45 @@
 <div class="container_12">
-    <div class="grid_12" id="header">
-      este es el header_ci donde puede ir un menu  
-    </div>
+    <div class="grid_5 prefix_7">
+	<link rel="stylesheet" href="<?php echo base_url();?>css/stylem.css" type="text/css" media="all">
+      
+        <ul id="nav">
+            <li><a href="http://www.script-tutorials.com/">Home</a></li>
+            <li><a href="http://www.script-tutorials.com/">Tutorials</a>
+                <div>
+                    <ul>
+                        <li><a href="http://www.script-tutorials.com/category/html-css/">HTML / CSS</a></li>
+                        <li><a href="http://www.script-tutorials.com/category/jquery/">JS / jQuery</a></li>
+                        <li><a href="http://www.script-tutorials.com/category/php/">PHP</a></li>
+                        <li><a href="http://www.script-tutorials.com/category/mysql/">MySQL</a></li>
+                        <li><a href="http://www.script-tutorials.com/category/xslt/">XSLT</a></li>
+                        <li><a href="http://www.script-tutorials.com/category/ajax/">Ajax</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="http://www.script-tutorials.com/category/resources/">Resources</a>
+                <div>
+                    <ul>
+                        <li><a href="http://www.script-tutorials.com/category/php/">PHP</a></li>
+                        <li><a href="http://www.script-tutorials.com/category/mysql/">MySQL</a></li>
+                        <li><a href="http://www.script-tutorials.com/category/xslt/">XSLT</a></li>
+                        <li><a href="http://www.script-tutorials.com/category/ajax/">Ajax</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="http://www.script-tutorials.com/category/resources/">Other pages</a>
+                <div>
+                    <ul>
+                        <li><a href="#">Page 1</a></li>
+                        <li><a href="#">Page 2</a></li>
+                        <li><a href="#">Page 3</a></li>
+                        <li><a href="#">Page 4</a></li>
+                        <li><a href="#">Page 5</a></li>
+                    </ul>
+                </div>
+            </li>
+         
+            <li class="pad"></li>
+        </ul>
+   
+
+	</div>
