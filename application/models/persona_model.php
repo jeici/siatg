3d6eccf5d55570a83f0bf3577bb7f07442596db9
JEYSSON <JEYSSON@JEYSSON-PC>
2013-06-11 23:02:01 -0600

    aqui esta la version del form de wufoo que no se porque falla y tambien el modelo de persona que no funciona

--- application/models/persona_model.php
+++ application/models/persona_model.php
@@ -2,11 +2,19 @@
 if (!defined('BASEPATH'))
 	exit('No direct script access allowed');
 class Persona_model extends CI_Model{
-	function agregar_persona($carnet_p, $id_carr, $id_tele, $name, $ape , $dir, $email, $status, $type){
+
+    function __construct(){
+		parent::__construct();
+		$this ->load->database();
+	}	
+    
+    
+    
+    function agregar_persona($carnet_p,  $name, $ape , $dir, $email, $status, $type){
 		$sql = "select prc_ins_persona('";
 		$sql.=$carnet_p."',";
-		$sql.=$id_carr."',";
-		$sql.=$id_tele."',";
+		//$sql.=$id_carr."',";
+		//$sql.=$id_tele."',";
 		$sql.=$name."',";
 		$sql.=$ape."',";
 		$sql.=$dir."',";
@@ -25,8 +33,11 @@ class Persona_model extends CI_Model{
 		else{
 			$data[]="la consulta no se pudo ejecutar";
 		return $data;
+                
 		} 
 			
 	}
+        
+        
 	
 }
--- application/views/form_persona.php
+++ application/views/form_persona.php
@@ -1,105 +1,153 @@
-<div class="container_12">
-    <div class="grid_12" id="prueba_ci">
-        <table border="0" align="center"  class="table table-condensed">
-        <?php 
-        $atributos = array('id'=> 'form_persona', 'class' => 'persona_form');
-        echo form_open('persona/datosenviados',$atributos); //en este caso datosenviados contiene la url que hace el action 
-        $data_carnet = array(
-              'name'        => 'carnet',
-              'id'          => 'id_carnet',
-              'value'       => '',
-              'maxlength'   => '7',
-              'size'        => '7',
-              'style'       => 'width:100%',
-              'placeholder' => 'Digite el carnet',
-            );
-        $data_nombres = array(
-              'name'        => 'nombres',
-              'id'          => 'id_nombre',
-              'value'       => '',
-              'maxlength'   => '30',
-              'size'        => '30',
-              'style'       => 'width:100%',
-              'placeholder' => 'Digite los nombres',
-            ); 
-        $data_apellidos = array(
-              'name'        => 'apellidos',
-              'id'          => 'id_apellidos',
-              'value'       => '',
-              'maxlength'   => '30',
-              'size'        => '30',
-              'style'       => 'width:100%',
-              'placeholder' => 'Digite los apellidos',
-            ); 
-        $data_dir = array(
-              'name'        => 'direccion',
-              'id'          => 'id_apellidos',
-              'value'       => '',
-              'width'       => '50',
-              'rows '       => '5',
-              'style'       => 'width:100%',
-              'maxlength'   => '30',
-              'placeholder' => 'Digite la dirección',
-            ); 
-        $data_correo = array(
-              'name'        => 'correo',
-              'id'          => 'id_correo',
-              'value'       => '',
-              'maxlength'   => '7',
-              'size'        => '7',
-              'style'       => 'width:100%',
-              'placeholder' => 'Digite el correo',
-            );
-        $data_checkbox = array(
-               'name'        => 'estado',
-               'id'          => 'newestado',
-               'value'       => 'accept',
-               'checked'     => TRUE,
-               'style'       => 'margin:10px',
-    );
-        $options = array(
-                  1  => 'Docente',
-                  2  => 'Estudiante',
-                  
-                );
+<!DOCTYPE html>
+<html>
+<head>
 
-        
-        
-        ?>
-        <tr>
-		<th colspan="6">DATOS PERSONALES</th>
-               
-	</tr>
-         
-        <tr>
-          <td> <?php echo form_label('Carnet:'); ?> </td>
-          <td> <?php echo form_input($data_carnet);?></td>
-          <td width="10"></td>
-          <td> <?php echo form_label('Nombres:');?></td>
-          <td> <?php echo form_input($data_nombres);?></td>
-          <td width="10"></td>
-          <td> <?php echo form_label('Apellidos:');?></td>
-          <td> <?php echo form_input($data_apellidos);?></td>
-        </tr> 
-        <tr>
-          <td> <?php echo form_label('Direccion:'); ?> </td>
-          <td> <?php echo form_textarea($data_dir);?></td>
-          <td width="10"></td>
-          <td> <?php echo form_label('Correo');?></td>
-          <td> <?php echo form_input($data_correo);?></td>
-          <td width="10"></td>
-          <td> <?php echo form_label('Estado:');?></td>
-          
-          <td> <?php echo form_checkbox($data_checkbox);?></td>
-          <td> <?php echo form_label('Activo');?></td>
-            
-        </tr> 
-        <tr>
-          <td> <?php echo form_label('Tipo');?></td>
-          <td> <?php echo form_dropdown('Tipo', $options,1);?></td>
-          <td> <?php echo form_submit('ingresar','Ingresar Persona');?></td>
-        </tr>
-       <?php echo form_close(); ?>  
-        </table>
-    </div>
+<title>
+Datos personales
+</title>
+
+<!-- Meta Tags -->
+<meta charset="utf-8">
+<meta name="generator" content="Wufoo">
+<meta name="robots" content="index, follow">
+
+<!-- CSS -->
+<link href="css/structure.css" rel="stylesheet">
+<link href="css/form.css" rel="stylesheet">
+
+<!-- JavaScript -->
+<script src="scripts/wufoo.js"></script>
+
+<!--[if lt IE 10]>
+<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+
+<body id="public">
+<div id="container" class="ltr">
+
+<h1 id="logo">
+<a href="http://wufoo.com" title="Powered by Wufoo">Wufoo</a>
+</h1>
+
+<form id="form1" name="form1" class="wufoo topLabel page" autocomplete="off" enctype="multipart/form-data" method="post" novalidate
+action="https://jrlopez.wufoo.com.mx/forms/z7x4m1/#public">
+
+<header id="header" class="info">
+<h2>Datos personales</h2>
+<div>Ingreso de Datos Personales</div>
+</header>
+
+<ul>
+
+<li id="foli1" class="notranslate      ">
+<label class="desc" id="title1" for="Field1">
+Carnet
+<span id="req_1" class="req">*</span>
+</label>
+<div>
+<input id="Field1" name="Field1" type="text" class="field text small" value="" maxlength="7" tabindex="1" onkeyup="validateRange(1, 'character');" required />
+<label for="Field1">Máximo permitido: <var id="rangeMaxMsg1">7</var> caracteres.&nbsp;&nbsp;&nbsp; <em class="currently">Utilizado actualmente: <var id="rangeUsedMsg1">0</var> caracteres.</em></label>
 </div>
+</li><li id="foli2" class="notranslate      ">
+<label class="desc" id="title2" for="Field2">
+Nombre
+<span id="req_2" class="req">*</span>
+</label>
+<span>
+<input id="Field2" name="Field2" type="text" class="field text fn" value="" size="8" tabindex="2" required />
+<label for="Field2">Nombre</label>
+</span>
+<span>
+<input id="Field3" name="Field3" type="text" class="field text ln" value="" size="14" tabindex="3" required />
+<label for="Field3">Apellidos</label>
+</span>
+</li>
+<li id="foli10" 
+class="notranslate      "><label class="desc" id="title10" for="Field10">
+Dirección
+<span id="req_10" class="req">*</span>
+</label>
+
+<div>
+<textarea id="Field10" 
+name="Field10" 
+class="field textarea medium" 
+spellcheck="true" 
+rows="10" cols="50" 
+tabindex="4" 
+onkeyup=""
+required  ></textarea>
+
+</div>
+</li>
+<li id="foli11" class="notranslate      ">
+<label class="desc" id="title11" for="Field11">
+Correo electrónico
+</label>
+<div>
+<input id="Field11" name="Field11" type="email" spellcheck="false" class="field text medium" value="" maxlength="255" tabindex="5" /> 
+</div>
+</li>
+<li id="foli12" class="notranslate      ">
+<fieldset>
+<![if !IE | (gte IE 8)]>
+<legend id="title12" class="desc">
+Estado
+</legend>
+<![endif]>
+<!--[if lt IE 8]>
+<label id="title12" class="desc">
+Estado
+</label>
+<![endif]-->
+<div>
+<span>
+<input id="Field12" name="Field12" type="checkbox" class="field checkbox" value="Activo" tabindex="6" />
+<label class="choice" for="Field12">Activo</label>
+</span>
+</div>
+</fieldset>
+</li><li id="foli213" class="notranslate       ">
+<label class="desc" id="title213" for="Field213">
+Tipo
+<span id="req_213" class="req">*</span>
+</label>
+<div>
+<select id="Field213" name="Field213" class="field select medium" tabindex="7" > 
+<option value="Docente" selected="selected">
+Docente
+</option>
+<option value="Esudiante" >
+Esudiante
+</option>
+<option value="Tercera opci&oacute;n" >
+Tercera opción
+</option>
+</select>
+</div>
+</li> <li class="buttons ">
+<div>
+
+                    <input id="saveForm" name="saveForm" class="btTxt submit" 
+    type="submit" value="Enviar"
+ /></div>
+</li>
+
+<li class="hide">
+<label for="comment">Do Not Fill This Out</label>
+<textarea name="comment" id="comment" rows="1" cols="1"></textarea>
+<input type="hidden" id="idstamp" name="idstamp" value="BHkEwxaor9w+kymJRpdV++wwKekqd+CX+f6zza+nYM0=" />
+</li>
+</ul>
+</form> 
+
+</div><!--container-->
+
+<a class="powertiny" href="http://wufoo.com/form-builder/" title="Powered by Wufoo"
+style="display:block !important;visibility:visible !important;text-indent:0 !important;position:relative !important;height:auto !important;width:95px !important;overflow:visible !important;text-decoration:none;cursor:pointer !important;margin:0 auto !important">
+<span style="background:url(./images/powerlogo.png) no-repeat center 7px; margin:0 auto;display:inline-block !important;visibility:visible !important;text-indent:-9000px !important;position:static !important;overflow: auto !important;width:62px !important;height:30px !important">Wufoo</span>
+<b style="display:block !important;visibility:visible !important;text-indent:0 !important;position:static !important;height:auto !important;width:auto !important;overflow: auto !important;font-weight:normal;font-size:9px;color:#777;padding:0 0 0 3px;">Designed</b>
+</a>
+</body>
+</html>
