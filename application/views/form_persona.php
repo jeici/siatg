6111a3ca98eb68158cd847c644ef460d36b579b1
jishi jeici <jishi.jeici@gmail.com>
2013-06-11 23:50:16 -0600

    dos modificaciones

--- application/views/form_persona.php
+++ application/views/form_persona.php
@@ -1,4 +1,29 @@
 
+<<<<<<< HEAD
+<title>
+Datos personales
+</title>
+
+<!-- Meta Tags -->
+<meta charset="utf-8">
+<meta name="generator" content="Wufoo">
+<meta name="robots" content="index, follow">
+
+<!-- CSS -->
+<link href="../css/structure.css" rel="stylesheet">
+<link href="../css/form.css" rel="stylesheet">
+
+<!-- JavaScript -->
+<script src="../scripts/wufoo.js"></script>
+
+<!--[if lt IE 10]>
+<script src="https://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+</head>
+
+<body id="public">
+=======
+>>>>>>> branch 'master' of https://github.com/jeici/siatg.git
 <div id="container" class="ltr">
 
     
--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -5,8 +5,15 @@
         <link href="../sigcenta/lib/blueprint/print.css" type="text/css" rel="stylesheet" media="print">
 		<link href="../sigcenta/lib/blueprint/estilo.css" type="text/css" rel="stylesheet">-->
 		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/style.css" media="screen"/>
-		<link rel="stylesheet" type="text/css" href="http://localhost:8080/siatg/css/960.css" media="screen"/>
-                <link rel="stylesheet" type="text/css" href="http://localhost:8080/siatg/css/reset.css" media="screen"/>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/960.css" media="screen"/>
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>css/reset.css" media="screen"/>
+		<!-- <link rel="stylesheet" type="text/css" href="http://localhost:8081/siatg/css/960.css" media="screen"/>
+        <link rel="stylesheet" type="text/css" href="http://localhost:8081/siatg/css/reset.css" media="screen"/>-->
+        <!-- <?php  //echo base_url();
+        			// pone la ruta de forma dinamica
+        			// esta ruta es la que extrae
+        			// http://localhost:8081/siatg/?> -->
+        
 
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<style>
