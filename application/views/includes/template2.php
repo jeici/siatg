2eba616778d4d023b63820bc5fee9aff0afc2442
Martin Espinal <Martin Espinal@Erebor>
2013-06-14 13:57:21 -0600

    modificado

--- application/views/includes/template2.php
+++ application/views/includes/template2.php
@@ -2,7 +2,9 @@
 // agrego la variable item para que me cargue el dropbox dentro de main_content de usuario
 $this->load->view('includes/header'); //aqui creamos el encabezado
 $this->load->view('includes/header_ci');
-$this->load->view($main_content,$item); //esta variable servira para pasar el contenido a la vista
+$llenado['item']=$item;
+$llenado['nivel']=$nivel;
+$this->load->view($main_content,$llenado); //esta variable servira para pasar el contenido a la vista
 $this->load->view('includes/footer');//esta variable carga el footer en la vista
 /*
  * To change this template, choose Tools | Templates
