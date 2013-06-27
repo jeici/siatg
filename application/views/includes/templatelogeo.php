0e6418806026dcc655bd2840c89e80177cb56ede
Jonathan <Jonathan@Jonathan-PC>
2013-06-26 18:53:56 -0600

    template modificado

--- application/views/includes/templatelogeo.php
+++ application/views/includes/templatelogeo.php
@@ -1,7 +1,7 @@
 <?php
 // agrego la variable item para que me cargue el dropbox dentro de main_content de usuario
 $this->load->view('includes/header'); //aqui creamos el encabezado
-$this->load->view('includes/header_ci');
+//$this->load->view('includes/header_ci');
 
 $llenado['logeo']=$logeo;
 $this->load->view($main_content,$llenado); //esta variable servira para pasar el contenido a la vista
